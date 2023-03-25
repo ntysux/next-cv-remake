16004c255dc3810e78795d0411781010d4aa9ba1
Duong Quoc Viet <ntysux@gmail.com>
2023-03-26 03:38:15 +0700

    custom & use theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,33 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  colors: {
+    app: {
+      black: {
+        1: '#49474B',
+        2: '#68666B'
+      },
+      gray: {
+        1: '#88898C',
+        2: '#D9D9D9',
+        3: '#E0E0E0',
+        4: '#EBEBEB'
+      },
+      teal: {
+        1: '#5CF2E3'
+      }
+    }
+  },
+  components: {
+    
+  }
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
