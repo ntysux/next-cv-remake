a9876c1d0f8bfd237f620e9e86ed9c862cdb0d13
Duong Quoc Viet <ntysux@gmail.com>
2023-03-26 16:39:06 +0700

    chore: add position=sticky when scroll

--- components/control-bar.tsx
+++ components/control-bar.tsx
@@ -5,7 +5,7 @@ import Settings from "./settings";
 
 export default function ControlBar() {
   return (
-    <HStack p='1'>
+    <HStack p='1' position='sticky' top='0'>
       <Settings />
       <Spacer />
       <Dashboard />
