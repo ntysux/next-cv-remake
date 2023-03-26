6bdae64e0dd350fb0ae5cbf816863ff40046dd15
Duong Quoc Viet <ntysux@gmail.com>
2023-03-26 10:38:31 +0700

    create control bar using Settings, Dashboard & CopyUrlToClipboard

---
+++ components/control-bar.tsx
@@ -0,0 +1,16 @@
+import { HStack, Spacer } from "@chakra-ui/react";
+import CopyUrlToClipboard from "./copy-url-to-clipboard";
+import Dashboard from "./dashboard";
+import Settings from "./settings";
+
+export default function ControlBar() {
+  return (
+    <HStack p='1'>
+      <Settings />
+      <Spacer />
+      <Dashboard />
+      <Spacer />
+      <CopyUrlToClipboard />
+    </HStack>
+  );
+}
