93fba78748c096ca1c0c4f88d9ce53d6650978f3
Duong Quoc Viet <ntysux@gmail.com>
2023-03-28 04:32:50 +0700

    create type interface of state

---
+++ redux/state-interface.ts
@@ -0,0 +1,44 @@
+export enum TypeData {
+  Heading,
+  List,
+  Note
+}
+
+export interface Heading {
+  id: string,
+  type: TypeData.Heading,
+  data: string
+}
+
+export interface Note {
+  id: string,
+  type: TypeData.Note,
+  data: string
+}
+
+export interface List {
+  id: string,
+  type: TypeData.List,
+  data: string[]
+}
+
+export type DataDisplay = Heading | Note | List
+
+export interface Section {
+  name: string,
+  data: DataDisplay[]
+}
+
+export interface Avatar {
+  name: string,
+  blodUrl: string,
+  notionUrl: string
+}
+
+export interface Cv {
+  id: string,
+  name: string,
+  color: string,
+  avatar?: Avatar,
+  section: Section[]
+}
