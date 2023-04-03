f8ccb84b18a428dd6f62138b053bf6ce8baf5e75
Duong Quoc Viet <ntysux@gmail.com>
2023-04-03 09:08:07 +0700

    get data from Notion api

--- pages/cv/[id].tsx
+++ pages/cv/[id].tsx
@@ -6,14 +6,16 @@ import { useSelector } from "react-redux";
 import StageView from "@/components/stage-view";
 import { GetServerSidePropsContext } from "next";
 import { Client } from "@notionhq/client";
-import { GetPageResponse } from "@notionhq/client/build/src/api-endpoints";
+import { Cv, Heading, List, Note, TypeData } from "@/redux/state-interface";
 
 interface Props {
-  isPage: boolean
+  isPage: boolean,
+  cvApi: Cv
 }
 
-export default function CvPage({isPage}: Props) {
+export default function CvPage({isPage, cvApi}: Props) {
   const { mode } = useSelector((state: RootState) => state.cv);
+  console.log(cvApi)
 
   return isPage ? (
     <Flex
@@ -53,16 +55,69 @@ export default function CvPage({isPage}: Props) {
 export async function getServerSideProps(context: GetServerSidePropsContext) {
   const pageId = context.resolvedUrl.split('/')[2];
   const notion = new Client({auth: `${process.env.NOTION_KEY}`});
-  let page: GetPageResponse | undefined;
+  let page: any;
+  let cvApi: Cv | undefined;
 
   // Check page id
   try {
-    page = await notion.pages.retrieve({ page_id: pageId });
+    page = await notion.pages.retrieve({page_id: pageId});
   } catch (error) {}
 
   if(page?.id) {
+    // get page data
+    const name: string = page.properties.name.title[0].plain_text;
+    const color: string = page.properties.color.rich_text[0].plain_text; 
+    
+    cvApi = {
+      id: page.id,
+      name: name,
+      color: color,
+      mode: false,
+      section: []
+    }
+
+    // get section page data
+    const sectionPage = await notion.databases.query({
+      database_id: `${process.env.NOTION_DATABASE_SECTION_ID}`,
+      filter: {
+        property: 'cvId',
+        rich_text: {
+          contains: page.id,
+        }
+      }
+    });
+
+    sectionPage.results.reverse().map((section: any) => {
+      if(section.properties.type.number === TypeData.Note) {
+        const data = section.properties.note.rich_text[0];
+        const sectionApi: Note = {
+          id: section.id,
+          type: TypeData.Note,
+          data: data ? data.plain_text : ''
+        };
+        cvApi?.section.push(sectionApi);
+      }
+      else if(section.properties.type.number === TypeData.Heading) {
+        const data = section.properties.heading.rich_text[0];
+        const sectionApi: Heading = {
+          id: section.id,
+          type: TypeData.Heading,
+          data: data ? data.plain_text : ''
+        };
+        cvApi?.section.push(sectionApi);
+      } else if(section.properties.type.number === TypeData.List) {
+        const data = section.properties.list.rich_text[0];
+        const sectionApi: List = {
+          id: section.id,
+          type: TypeData.List,
+          data: data ? data.plain_text : ''
+        };
+        cvApi?.section.push(sectionApi);
+      }
+    });
+
     return {
-      props: {isPage: true}
+      props: {isPage: true, cvApi}
     }
   }
   else {
